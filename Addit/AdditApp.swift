1d975d9d1e297fac150625ba308a665abb190022
ClintTLang <clinton.t.lang@vanderbilt.edu>
2026-03-09 19:36:22 -0700

    Initial Commit

---
+++ Addit/AdditApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdditApp.swift
+//  Addit
+//
+//  Created by Clint Lang on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct AdditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Addit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Addit
+//
+//  Created by Clint Lang on 3/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
